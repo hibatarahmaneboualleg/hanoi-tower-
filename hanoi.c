6a989c6623e1d690f02d909c5607583037a251e4
Boualleg Hibat arrahman <hibaboub88@gmail.com>
2025-02-22 22:47:03 +0100

    Add files via upload

---
+++ hanoi.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+const char* getDiskSize(int n) {
+    const char* sizes[] = {"small", "medium", "large", "huge"}; 
+    return sizes[n - 1];
+}
+
+void hanoi(int n, char* FIRST, char* THIRD, char* SECOND) {
+    if (n == 1) {
+        printf("Put the %s disk from |%s| to |%s|.\n", getDiskSize(n), FIRST, THIRD);
+    } else {
+        hanoi(n - 1, FIRST, SECOND, THIRD);
+        printf("Put the %s disk from |%s| to |%s|\n", getDiskSize(n), FIRST, THIRD);
+        hanoi(n - 1, SECOND, THIRD, FIRST);
+    }
+}
+
+int main() {
+    int n = 3; 
+    hanoi(n, "FIRST", "LAST", "SECOND"); 
+    return 0;
+}
